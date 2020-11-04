d1b5cbfcf1011598e40c6ced44067f7c4a7d18e7
sandraglokken <59877151+sandraglokken@users.noreply.github.com>
2020-11-04 13:17:13 +0100

    Fungerende spill uten regulator

--- pingpong.c
+++ pingpong.c
@@ -10,17 +10,26 @@
 
 void pingpong_play(void){
 	Message message;
+	message.data[0]=0;
+	message.data[1]=0;
+	message.length=2;	
+	message.ID = 101;
+	CAN_send_message(&message);
+	
+	message.ID =0;
 	
 	while (adc_joystick_dir(&joystick)!=BACK){
 		adc_joystick_pos(&joystick);
 		message.data[0]=joystick.x;
 		message.data[1]=joystick.y;
-		message.length=2;		
+		message.data[2]= adc_read_button_touch_r();
+		message.length=3;		
 		CAN_send_message(&message);
-		printf("X: %d \n \r", message.data[1]);
+		printf("button: %d \n \r", message.data[2]);
 	}
 	message.data[0]=0;
 	message.data[1]=0;
+	message.data[2]=0;
 	message.length=2;
 	CAN_send_message(&message);
 }
