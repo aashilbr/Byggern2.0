85ed5f95fc7dd8c6ef1f7bf1ece617876c92828b
andreaschnell <59877162+andreaschnell@users.noreply.github.com>
2020-11-15 16:06:51 +0100

    Added docygen

--- Node_2/ir_signal.h
+++ Node_2/ir_signal.h
@@ -3,12 +3,21 @@
  *
  * Created: 21.10.2020 17:14:07
  *  Author: sandrgl
- */ 
+ */
 
 
 #ifndef IR_SIGNAL_H_
 #define IR_SIGNAL_H_
 #include "sam.h"
+
+//Ta med adc_init funskjnen her???
+
+
+/**
+* @brief Function for reading IR signal and convert analog data.
+* @return ADC channel 0 data register.
+* @details Function for analog to digital conversion, returns the converted data.
+*/
 uint16_t read_ir_signal();
 
 
