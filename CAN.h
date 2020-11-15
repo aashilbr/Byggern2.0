a591979b2fa3e900447193f7938db0770150397f
aashilbr <59867493+aashilbr@users.noreply.github.com>
2020-11-15 16:51:34 +0100

    Added doxygen

--- CAN.h
+++ CAN.h
@@ -1,9 +1,9 @@
-/*
- * CAN.h
- *
- * Created: 30.09.2020 14:00:55
- *  Author: andrschn
- */
+/**
+* @file CAN.h
+* @author andrschn
+* @date 30.09.2020 14:00:55
+* @brief Library of functions using the CAN interface.
+*/
 
 #include "MCP2515_driver.h"
 #include <avr/io.h>
@@ -28,6 +28,7 @@ typedef struct Message_s{
 * @brief Initializes CAN.
 * @param [in] mode Operation mode of the CAN controller.
 * @details Sets the operation mode and configures the CAN bus.
+* Enables interrupts.
 */
 uint8_t CAN_init(uint8_t mode);
 
@@ -42,18 +43,36 @@ void CAN_send_message(Message *message);
 * @return Returns 1 if the buffer is pending transmission, 0 otherwise.
 */
 uint8_t CAN_transmit_request(void);
+
 /**
 * @brief Receives a message over the CAN bus.
-* @param [in] message A pointer to a Message object. 
+* @param [in] message A pointer to a Message object.
 */
 void CAN_receive_message(Message *message);
 
+/**
+* @brief Sends message over the CAN bus that the
+easy PID controller shall be used in the game.
+*/
 void oled_easy_pid(void);
 
+/**
+* @brief ends message over the CAN bus that the
+hard PID controller shall be used in the game.
+*/
 void oled_hard_pid(void);
 
+/**
+* @brief Checks if there has happened a CAN interrupt.
+*/
 int check_CAN_interrupt(void);
 
-CAN_send_pos(int8_t x, int8_t y,uint8_t button );
+/**
+* @brief Sends the position of the x, y and button values over the Can bus.
+*@param [in] x The x-position.
+*@param [in] y The y-position.
+*@param [in] button The button value.
+*/
+void CAN_send_pos(int8_t x, int8_t y,uint8_t button );
 
 #endif /* CAN_H_ */
