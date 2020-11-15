becee8d513f6069c37a18a9c6ad9a10305413f3c
aashilbr <59867493+aashilbr@users.noreply.github.com>
2020-11-15 14:54:54 +0100

    Added doxygen

--- timer.h
+++ timer.h
@@ -3,7 +3,7 @@
  *
  * Created: 11.11.2020 11:22:32
  *  Author: sandrgl
- */ 
+ */
 
 
 #ifndef TIMER_H_
@@ -11,16 +11,36 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+/**
+* Timer:
+* @count An integer containing how long the timer has been counting, in seconds.
+* @details The variable counts how many times the timer calls the interrupt
+* service routine.
+*/
 typedef struct Timer_s{
 	int count;
 }Timer;
 
 Timer timer;
 
+/**
+* @brief Initializes the timer
+* @details Starts the timer. Sets clock prescalar to  64, enables interrupts and
+* compare match on 90.
+*/
 void timer_init(void);
 
+/**
+* @brief Starts the timer
+* @details Initializes the timer count to be 0.
+*/
 void start_timer(void);
 
+/**
+* @brief Retrieves the timer count.
+* @return The value the timer has counted to.
+*/
 uint8_t get_count(void);
 
 #endif /* TIMER_H_ */
