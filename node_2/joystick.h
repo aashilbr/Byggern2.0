291750f7d0226126aaa47a9951d6a148a08cbc5f
andreaschnell <59877162+andreaschnell@users.noreply.github.com>
2020-11-16 19:59:58 +0100

    Added doxygen

--- node_2/joystick.h
+++ node_2/joystick.h
@@ -1,9 +1,9 @@
-/*
- * joystick.h
- *
- * Created: 14.10.2020 09:32:51
- *  Author: sandrgl
- */ 
+/**
+* @file node_2/joystick.h
+* @author sandrgl
+* @date  14.10.2020
+* @breif Struct with joystick state
+*/
 
 #ifndef JOYSTICK_H_
 #define JOYSTICK_H_
@@ -11,11 +11,15 @@
 #include "sam.h"
 #include <stdlib.h>
 
+/**
+* @brief A struct for saving the joystick position and the shoot-button state
+*/
 typedef struct{
-	int8_t x;
-	int8_t y;
-	int8_t shoot;
+	int8_t x; /**< The x position of the joystick, between -100 and 100 */
+	int8_t y; /**< The y position of the joystick, between -100 and 100 */
+	int8_t shoot; /**< Is 1 if button to shoot is pushed and 0 otherwise */
 }pos_js;
+
 
 
 pos_js js_pos;
