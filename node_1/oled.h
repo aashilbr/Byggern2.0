70b8d59ae556c9d45972952fde09191583b8ae3e
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2020-11-16 17:03:21 +0100

    take oled functions out of menu
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- node_1/oled.h
+++ node_1/oled.h
@@ -6,10 +6,7 @@
  */
 #ifndef OLED_H_
 #define OLED_H_
-#define OLED_BASE_C_ADDRESS 0x1000
-#define OLED_BASE_D_ADDRESS 0x1300
-
-
+#include "menu.h"
 #include <stdint.h>
 
 
@@ -17,7 +14,6 @@
 * @brief Initializes the OLED
 */
 void oled_init();
-
 
 /**
 * @brief Selects the page to start writing from.
@@ -60,5 +56,45 @@ void oled_pos(uint8_t line, uint8_t column);
 * @param [in] data Data to print to the OLED screen.
 */
 void oled_print(char * data);
+
+/**
+ * @brief Print a menu node on the OLED display. the
+ * line indicated by @p selected_item will be selected.
+ *
+ * @p p_node Points to the root of a (sub)menu. Its
+ * @c name parameter will be used as the title of the
+ * menu screen, while all its children will be printed
+ * underneath. Navigation is handled independently by
+ * the rest of the application code to decouple the
+ * menu system from the rest of the code base.
+ *
+ * @param[in] p_node Pointer to a MenuNode that is to
+ * be displayed on the OLED.
+ * @param selected_item The selected item in the menu.
+ * This parameter is zero-indexed, and goes all the way
+ * up to the number of children in the menu - 1.
+ *
+ * @warning A call to @c oled_init must be invoked
+ * before this function will operate as expected.
+ */
+void oled_print_menu(const MenuNode * p_node, uint8_t selected_item);
+
+/**
+ * @brief Set the brightness level of the OLED
+ * display to the minimum possible setting.
+ */
+void oled_set_brightness_minimum();
+
+/**
+ * @brief Set the brightness level of the OLED
+ * display to a medium setting.
+ */
+void oled_set_brightness_medium();
+
+/**
+ * @brief Set the brightness level of the OLED
+ * display to the maximum possible setting.
+ */
+void oled_set_brightness_maximum();
 
 #endif /* OLED_H_ */
