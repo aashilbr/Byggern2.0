248aafb59547c4178286c8c9a3d99f85ea17e885
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2020-11-16 15:50:28 +0100

    take menu system out of main
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- node_1/main.c
+++ node_1/main.c
@@ -21,151 +21,7 @@
 
 #include <avr/pgmspace.h>
 
-static void menu_no_action(void){
-	//oled_print("Activation \n");
-	
-}
 
-static Menu_node m_menu_nodes[]={
-	{
-		"MAIN MENU",
-		NULL,
-		m_menu_nodes + 1,
-		NULL,
-		oled_menu_main
-	},
-	{
-		"GAMES",
-		m_menu_nodes + 0,
-		m_menu_nodes + 3,
-		m_menu_nodes + 2,
-		menu_no_action
-	},
-	{
-		"SETTINGS",
-		m_menu_nodes + 0,
-		m_menu_nodes + 5,
-		NULL,
-		menu_no_action
-	},
-	{
-		"Tic Tac Toe",
-		m_menu_nodes + 1,
-		NULL,
-		m_menu_nodes + 4,
-		ttt_menu_game
-	},
-	{
-		"Ping Pong",
-		m_menu_nodes + 1,
-		m_menu_nodes + 8,
-		NULL,
-		menu_no_action
-	},
-	{
-		"Brightness",
-		m_menu_nodes + 2,
-		m_menu_nodes + 10,
-		m_menu_nodes + 6,
-		menu_no_action
-	},
-	{
-		"Set Difficulty",
-		m_menu_nodes + 2,
-		m_menu_nodes + 13,
-		m_menu_nodes + 7,
-		menu_no_action
-	},
-	{
-		"Calibrate",
-		m_menu_nodes + 2,
-		NULL,
-		NULL,
-		oled_calibrate_joystick
-	},
-	{
-		"Steering",
-		m_menu_nodes + 4,
-		m_menu_nodes + 15,
-		m_menu_nodes + 9,
-		menu_no_action
-	},
-	{
-		"High score",
-		m_menu_nodes + 4,
-		m_menu_nodes + 17,
-		NULL,
-		oled_menu_high_score_sub
-	},
-	{
-		"MIN",
-		m_menu_nodes + 5,
-		NULL,
-		m_menu_nodes + 11,
-		oled_set_brightness_lvl_min
-	},
-	{
-		"Medium",
-		m_menu_nodes + 5,
-		NULL,
-		m_menu_nodes + 12,
-		oled_set_brightness_lvl_med
-	},
-	{
-		"MAX",
-		m_menu_nodes + 5,
-		NULL,
-		NULL,
-		oled_set_brightness_lvl_max
-	},
-	{
-		"Easy",
-		m_menu_nodes + 6,
-		NULL,
-		m_menu_nodes + 14,
-		oled_easy_pid
-	},
-	{
-		"Hard",
-		m_menu_nodes + 6,
-		NULL,
-		NULL,
-		oled_hard_pid
-	},
-	{
-		"Joystick steering",
-		m_menu_nodes + 8,
-		NULL,
-		m_menu_nodes + 16,
-		pingpong_play_with_joystick
-	},
-	{
-		"Slider steering",
-		m_menu_nodes + 8,
-		NULL,
-		m_menu_nodes + 18,
-		pingpong_play_with_sliders
-	},
-	{
-		"Reset highscore",
-		m_menu_nodes + 9,
-		NULL,
-		NULL,
-		reset_highscore
-	},
-	{
-		"Memory program",
-		m_menu_nodes + 8,
-		NULL,
-		NULL,
-		pingpong_move_with_memory
-	}
-};
-
-
-const Menu_node * menu_root_node(){
-	return m_menu_nodes;
-}
 
 Grid_state game_board[]={NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE};
 
@@ -185,17 +41,14 @@ int main(void){
 	adc_calibrate_joystick(&joystick);
 	state current_state;
 	current_state.current_node=menu_root_node();
-	menu_init(&current_state);	
+	menu_init(&current_state);
 	adc_calibrate_joystick(&joystick);
-	
+
 	while(1){
-		
+
 		adc_joystick_pos(&joystick);
 		oled_navigate(adc_joystick_dir(&joystick), &current_state);
-		
+
 	}
  	return 0;
 }
-
-
-
