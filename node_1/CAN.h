c9f54d0a8090a9370be2b9b89dec774e241c09fd
sandraglokken <59877151+sandraglokken@users.noreply.github.com>
2020-11-16 20:08:51 +0100

    documented all functions

--- node_1/CAN.h
+++ node_1/CAN.h
@@ -1,9 +1,10 @@
-/*
- * CAN.h
- *
- * Created: 30.09.2020 14:00:55
- *  Author: andrschn
- */
+
+/**
+* @file node_1/CAN.h
+* @author andrschn
+* @date 30.09.2020 14:00:55
+* @brief For CAN communication
+*/
 
 #include "MCP2515_driver.h"
 #include <avr/io.h>
@@ -44,16 +45,37 @@ void CAN_send_message(Message *message);
 uint8_t CAN_transmit_request(void);
 /**
 * @brief Receives a message over the CAN bus.
-* @param [in] message A pointer to a Message object. 
+* @param [in] message A pointer to a Message object.
 */
 void CAN_receive_message(Message *message);
 
+
+/**
+* @brief Setting the pid factors to the best tuning -->easy mode
+* @details Sends a CAN message with the
+* specified message content for the easy pid setting
+*/
 void oled_easy_pid(void);
 
+/**
+* @brief Setting the pid factors to the best tuning -->hard mode
+* @details Sends a CAN message with the
+* specified message content for the hard pid setting
+*/
 void oled_hard_pid(void);
 
+/**
+* @brief Checks if the CAN interrupt flag is set
+* @return Returns 1 if the flag is set and 0 if not
+*/
 int check_CAN_interrupt(void);
 
+/**
+* @brief Sends a position and button information over the CAN bus
+* @param [in] x x-position
+* @param [in] y y-position
+* @param [in] button the button state, 1 for pressed
+*/
 void CAN_send_pos(int8_t x, int8_t y,uint8_t button );
 
 #endif /* CAN_H_ */
