4f42ac7cc953a44ac91a5204266d9520378f2c22
aashilbr <59867493+aashilbr@users.noreply.github.com>
2020-11-16 22:31:17 +0100

    Removed unecessary comments

--- node_2/pingpong_states.c
+++ node_2/pingpong_states.c
@@ -3,7 +3,7 @@
  *
  * Created: 28.10.2020 11:39:25
  *  Author: sandrgl
- */ 
+ */
 #include "pingpong_states.h"
 #include "can_controller.h"
 
@@ -15,7 +15,6 @@ void init_ppstate(){
 void count_score(void){
 	if(pingpong_state.ball_detected==0 && read_ir_signal()<THRESHOLD_ADC){
 		pingpong_state.score++;
-		printf("Count : %d \n", pingpong_state.score);
 		pingpong_state.ball_detected=1;
 	}
 	if (read_ir_signal()>THRESHOLD_ADC){
@@ -31,7 +30,6 @@ void check_if_game_over(void){
 		message.data_length = 1;
 		message.data[0] = 100;
 		while(can_send(&message,0));
-		//while(can_send(&message,1));
 		pingpong_state.ball_detected=1;
 	}
 }
