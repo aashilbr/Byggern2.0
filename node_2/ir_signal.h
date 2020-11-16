cfc85dd8ea4cd643fceefb761c99f6262932c6fc
Andrea <andreaschnell@outlook.com>
2020-11-16 19:32:42 +0100

    Added doxygen
    Signed-off-by: Andrea <andreaschnell@outlook.com>

--- node_2/ir_signal.h
+++ node_2/ir_signal.h
@@ -1,9 +1,9 @@
-/*
- * ir_signal.h
- *
- * Created: 21.10.2020 17:14:07
- *  Author: sandrgl
- */ 
+/**
+* @file Node2/ir_signal.h
+* @author sandrgl
+* @date 21.10.2020
+* @brief Code for coverting analog IR signal to digital signal
+*/
 
 
 #ifndef IR_SIGNAL_H_
@@ -13,4 +13,12 @@ uint16_t read_ir_signal();
 
 
 
+//Ta med adc_init funskjnen her???
+
+
+/**
+* @brief Function for reading IR signal and convert analog data.
+* @return ADC channel 0 data register.
+* @details Function for analog to digital conversion, returns the converted data.
+*/
 #endif /* IR_SIGNAL_H_ */
