4c2ce41d0c7cb117f120266350e432309e98dbb4
Sandra Garder Løkken <andrschn@stud.ntnu.no>
2020-11-16 21:04:15 +0100

    Added oled functions from menu.*
    Signed-off-by: Sandra Garder Løkken <andrschn@stud.ntnu.no>

--- node_1/oled.c
+++ node_1/oled.c
@@ -89,3 +89,137 @@ void oled_print(char* data){
 		}
 	}
 }
+
+
+
+void oled_frame(uint8_t line){
+	oled_pos(line,2);
+	oled_write_data(0xff);
+	oled_pos(line+1,2);
+	for(int i=3;i<127;i++){
+		oled_write_data(0x01);
+	}
+	oled_pos(line-1,2);
+	for(int i=3;i<127;i++){
+		oled_write_data(0x80);
+	}
+	oled_pos(line,126);
+	oled_write_data(0xff);
+}
+
+void oled_clear_frame(uint8_t line){
+	oled_clear_line(line-1);
+	oled_clear_line(line+1);
+	oled_pos(line,2);
+	oled_write_data(0x00);
+	oled_pos(line,126);
+	oled_write_data(0x00);
+}
+
+
+void oled_set_brightness_lvl(uint8_t lvl){
+	write_c(0x81);
+	write_c(lvl);
+}
+
+void oled_menu_set_brightness(void) {
+	oled_clear_all();
+	oled_pos(0,4);
+	oled_print("BRIGHTNESS:");
+	oled_pos(2,4);
+	oled_print("Min");
+	oled_pos(4,4);
+	oled_print("Med");
+	oled_pos(6,4);
+	oled_print("Max");
+}
+
+
+void oled_menu_main(void) {
+	oled_clear_all();
+	oled_pos(0,4);
+	oled_print("MAIN MENU:");
+	oled_pos(2,4);
+	oled_print("Games");
+	oled_pos(4,4);
+	oled_print("Settings");
+}
+
+void oled_menu_games(void) {
+	
+	oled_clear_all();
+	oled_pos(0,4);
+	oled_print("GAMES:");
+	oled_pos(2,4);
+	oled_print("Tic Tac Toe");
+	oled_pos(4,4);
+	oled_print("Ping Pong");
+}
+
+void oled_menu_settings(void) {
+	
+	oled_clear_all();
+	oled_pos(0,4);
+	oled_print("SETTINGS:");
+	oled_pos(2,4);
+	oled_print("Brightness");
+	oled_pos(4,4);
+	oled_print("Difficulty");
+	oled_pos(6,4);
+	oled_print("Calibrate");
+}
+
+void oled_menu_games_sub(void) {
+	
+	oled_clear_all();
+	oled_pos(0,4);
+	oled_print("GAMES:");
+	oled_pos(2,4);
+	oled_print("Play");
+	oled_pos(4,4);
+	oled_print("High score");
+}
+
+void oled_menu_difficulty(void) {
+	oled_clear_all();
+	oled_pos(0,4);
+	oled_print("DIFFICULTY:");
+	oled_pos(2,4);
+	oled_print("Easy");
+	oled_pos(4,4);
+	oled_print("Hard");
+}
+
+void oled_set_brightness_lvl_min(void){
+	oled_set_brightness_lvl(0x00);
+	oled_clear_all();
+	oled_pos(2,23);
+	oled_print("THERE YOU GO :)");
+}
+void oled_set_brightness_lvl_med(void){
+	oled_set_brightness_lvl(0x7e);
+	oled_clear_all();
+	oled_pos(2,23);
+	oled_print("THERE YOU GO :)");
+}
+void oled_set_brightness_lvl_max(void){
+	oled_set_brightness_lvl(0xff);
+	oled_clear_all();
+	oled_pos(2,23);
+	oled_print("THERE YOU GO :)");
+}
+
+
+
+void oled_menu_stearing(void) {
+	oled_clear_all();
+	oled_pos(0,4);
+	oled_print("Stearing mechanism:");
+	oled_pos(2,4);
+	oled_print("Joystick");
+	oled_pos(4,4);
+	oled_print("Sliders");
+	oled_pos(6,4);
+	oled_print("Memory");
+}
+
